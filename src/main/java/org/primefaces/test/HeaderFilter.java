469913cd2d7b33bd740ee1e3a9ea0a78f5b89ff7
RonR <RRuffin@nciinc.com>
2024-10-18 13:44:59 -0400

    Captured null subjectDN issue
    If I open browser and navigate quickly to app, I get error.  If I then reload page, I get normal welcome page.

---
+++ src/main/java/org/primefaces/test/HeaderFilter.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package org.primefaces.test;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+public class HeaderFilter implements Filter {
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {}
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        HttpServletRequest httpRequest = (HttpServletRequest) request;
+        HttpServletResponse httpResponse = (HttpServletResponse) response;
+
+        // Simulate SubjectDN header
+        httpResponse.setHeader("MYAPP_X-CertSubj", "cn=Bob.Davis,ou=ANNOUNCER,ou=PKI,ou=Com,o=WGMS,c=US");
+
+        chain.doFilter(httpRequest, httpResponse);
+    }
+
+    @Override
+    public void destroy() {}
+}
---
+++ src/main/java/org/primefaces/test/LoginBean.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package org.primefaces.test;
+
+import java.io.IOException;
+import javax.enterprise.context.RequestScoped;
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
+import javax.inject.Named;
+import javax.servlet.http.HttpServletRequest;
+
+@Named
+@RequestScoped
+public class LoginBean {
+    private final String INDEX_URL = FacesContext.getCurrentInstance().getExternalContext().getRequestContextPath() + "/mainpage.xhtml";
+
+    public void login() throws IOException {
+        HttpServletRequest request = (HttpServletRequest) FacesContext.getCurrentInstance().getExternalContext().getRequest();
+        String subjectDN = request.getHeader("MYAPP_X-CertSubj");
+
+        if (subjectDN == null) {
+            FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR, "Login Error", "SubjectDN is null. Please try again."));
+            return;
+        }
+
+        FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, "Login Successful", "Welcome! " + subjectDN));
+        FacesContext.getCurrentInstance().getExternalContext().redirect(INDEX_URL);
+    }
+}
